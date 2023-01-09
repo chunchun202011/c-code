e273745ad82449b9cdb84b4046c880cdfb572d7b
chunchun202011 <2530043704@qq.com>
2023-01-09 21:30:11 +0800

    day1.9

---
+++ game1/game.h
@@ -0,0 +1,14 @@
+#define ROW 3
+#define COL 3
+
+//函数声明
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL],int row,int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+//告诉我们4种状态
+//玩家赢 - '*'
+// 电脑赢 - '#'
+// 平局 - 'Q'
+// 继续 - 'C'
+char IsWin(char board[ROW][COL], int row,int col);
